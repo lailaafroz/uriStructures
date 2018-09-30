67cb227ec72731932878079f263aa09536457275
lailaafroz <35794882+lailaafroz@users.noreply.github.com>
2018-09-30 13:13:28 +0600

    Add files via upload

---
+++ uri1022.cpp
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+void lessratio(int &a,int &b){
+	
+	int temp  = max(a,b);
+	
+	for(int i=temp; i>=1 ;i--){
+		if((!(a%i)) && (!(b%i))){
+			a = a/i;
+			b = b/i;
+		}
+	}
+}
+
+int main(){
+
+    int tc,temp;
+    cin >> tc;
+    for(int x=0; x<tc && tc >=1 && tc <= 10000; x++){
+    
+      int  n1,n2,d1,d2;
+      char c2;
+      int  value[4];
+      string  str[3];
+        
+       for(int i=0; i<3; i++){
+              cin >> value[i];
+              cin >> str[i];
+       }
+       cin >> d2;
+       
+       n1 = value[0];
+       d1 = value[1];
+       n2 = value[2];
+    	
+      c2 = str[1][0];
+      
+      //cout << c2;	
+    		
+      if(c2=='+'  || c2=='-'){
+      
+        lessratio(n1,d1);
+        lessratio(n2,d2);
+    
+        temp = d1;
+        d1 *= d2;	
+        n1 *= d2;
+        n2 *= temp;
+        d2 *= temp;
+        if(c2=='+'){
+            n1 += n2;
+        }
+        else {
+            n1 -= n2;
+        }
+    }
+    else if(c2=='*'){
+        n1 *= n2;
+        d1 *= d2;
+    }
+    else if(c2=='/'){
+        n1 *= d2;
+        d1 *= n2;
+    }
+
+    n2 = n1;
+    d2 = d1;
+    
+    lessratio(n2,d2);
+    
+    cout << n1 << "/" << d1 << " = " << n2 << "/" << d2 << endl;
+    }
+    return 0;
+}
